d04f739c620c03858695eb1162aaefa907e55b7e
root <fdgomez@fi.uba.ar>
2021-05-16 16:13:11 +0000

    Actualizo pbm_test. Todavia no abre los archivos

--- test/pbm_test.c
+++ test/pbm_test.c
@@ -6,8 +6,18 @@
 #include<stdlib.h>
 #include<string.h>
 
+#define EQUALS 0
+
+#define RED "\x1B[31m"
+#define GREEN "\x1B[32m"
+#define RESET "\x1B[0m"
+
 //Precondicion: todos los archivos en el directorio deben ser .pbm
 int file_select(const struct dirent* entry);
+
+int pbm_cmp(FILE* file_1, FILE* file_2);
+
+void run_test(struct dirent** files,int n);
 
 int main(int argc, char* argv[]){
 
@@ -17,11 +27,9 @@ int main(int argc, char* argv[]){
 	n = scandir("../files/output",&files,file_select,alphasort);
 	if(n<0) perror("scandir");
 	else{
-		int i = n;
-		for(int i=0;i<n;i++){
-			printf("\n%s\n",files[i]->d_name);
 		
-		}
+		run_test(files,n);
+
 		for(int i=0;i<n;i++){
 			free(files[i]);
 		}
@@ -30,6 +38,49 @@ int main(int argc, char* argv[]){
 	}
 
 	return 0;
+}
+
+void run_test(struct dirent** files, int n){
+	int passed = 0;
+	int errors = 0;
+	int not_open = 0;
+	printf("\n\nRunning test...  %d files\n\n",n);
+	sleep(1);
+	for(int i=0;i<n;i=i+2){
+		
+		//Tira error porque no puedo abrir los pbm
+		FILE* file_1 = fopen(files[i]->d_name,"r");
+		FILE* file_2 = fopen(files[i+1]->d_name,"r");
+		
+		if(file_1==NULL || file_2==NULL){
+			printf(RED"Could not open files\n"RESET);
+			not_open++;
+		}
+		else{
+			if(pbm_cmp(NULL,NULL/*file_1,file_2*/)== EQUALS){
+				printf(GREEN "[PASS]" RESET);
+				passed++;
+			}
+			else{
+				printf(RED"[ERROR]"RESET);
+				errors++;
+			}
+			printf(": %s - %s \n",files[i]->d_name,files[i+1]->d_name);
+			sleep(1);
+		}
+			if(file_1!=NULL) fclose(file_1);
+			if(file_2!=NULL) fclose(file_2);
+
+	}
+	printf(GREEN "\nPASSED:%d" RESET ,passed);
+	printf(RED "\tERRORS:%d" RESET,errors);
+	printf(RED "\tFILES NOT OPEN:%d\n\n"RESET,not_open);
+	return;
+}
+
+int pbm_cmp(FILE* file_1, FILE* file_2){
+
+	return 0; //retornar siempre que son iguales por ahora
 }
 
 
