a616e0f7111a9e8f0355a2e8ab9a247bd977a9e3
root <fdgomez@fi.uba.ar>
2021-05-19 00:17:20 +0000

    Actualizo. Ya funcionan los test

--- TP1/scripts/pbm_files_c.sh
+++ TP1/scripts/pbm_files_c.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 ./../bin/autcel\
-	126 50 ../files/input/input_50.txt ../files/output/out_126_50_c.pbm
+	126 50 ../files/input/input_50.txt -o ../files/output/out_126_50_c
 
 ./../bin/autcel\
-       	110 50 ../files/input/input_50.txt ../files/output/out_110_50_c.pbm 
+       	110 50 ../files/input/input_50.txt -o ../files/output/out_110_50_c 
  
 ./../bin/autcel\
-       	30 50 ../files/input/input_50.txt ../files/output/out_30_50_c.pbm 
+       	30 50 ../files/input/input_50.txt -o ../files/output/out_30_50_c 
  
 ./../bin/autcel\
-       	126 80 ../files/input/input_80.txt ../files/output/out_126_80_c.pbm
+       	126 80 ../files/input/input_80.txt -o ../files/output/out_126_80_c
 
 ./../bin/autcel\
-       	110 80 ../files/input/input_80.txt ../files/output/out_110_80_c.pbm  
+       	110 80 ../files/input/input_80.txt -o ../files/output/out_110_80_c  
 
 ./../bin/autcel\
-       	30 80 ../files/input/input_80.txt ../files/output/out_30_80_c.pbm
+       	30 80 ../files/input/input_80.txt -o ../files/output/out_30_80_c
--- TP1/scripts/pbm_files_s.sh
+++ TP1/scripts/pbm_files_s.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 ./../bin/autcel\
-	126 50 ../files/input/input_50.txt ../files/output/out_126_50_s.pbm
+	126 50 ../files/input/input_50.txt -o ../files/output/out_126_50_s
 
 ./../bin/autcel\
-       	110 50 ../files/input/input_50.txt ../files/output/out_110_50_s.pbm 
+       	110 50 ../files/input/input_50.txt -o ../files/output/out_110_50_s 
  
 ./../bin/autcel\
-       	30 50 ../files/input/input_50.txt ../files/output/out_30_50_s.pbm 
+       	30 50 ../files/input/input_50.txt -o ../files/output/out_30_50_s 
  
 ./../bin/autcel\
-       	126 80 ../files/input/input_80.txt ../files/output/out_126_80_s.pbm
+       	126 80 ../files/input/input_80.txt -o ../files/output/out_126_80_s
 
 ./../bin/autcel\
-       	110 80 ../files/input/input_80.txt ../files/output/out_110_80_s.pbm  
+       	110 80 ../files/input/input_80.txt -o ../files/output/out_110_80_s  
 
 ./../bin/autcel\
-       	30 80 ../files/input/input_80.txt ../files/output/out_30_80_s.pbm
+       	30 80 ../files/input/input_80.txt -o ../files/output/out_30_80_s
