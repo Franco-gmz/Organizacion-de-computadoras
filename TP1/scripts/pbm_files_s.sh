54844fd2ed489af1ce4d151fa19531982866200d
root <fdgomez@fi.uba.ar>
2021-05-19 13:06:12 +0000

    Agrego pruebas

--- TP1/scripts/pbm_files_s.sh
+++ TP1/scripts/pbm_files_s.sh
@@ -16,3 +16,24 @@
 
 ./../bin/autcel\
        	30 80 ../files/input/input_80.txt -o ../files/output/out_30_80_s
+
+./../bin/autcel\
+	13 50 ../files/input/input_50.txt -o ../files/output/out_13_50_s
+
+./../bin/autcel\
+	0 50 ../files/input/input_50.txt -o ../files/output/out_0_50_s
+
+./../bin/autcel\
+	15 50 ../files/input/input_50.txt -o ../files/output/out_15_50_s
+
+./../bin/autcel\
+	29 50 ../files/input/input_50.txt -o ../files/output/out_29_50_s
+
+./../bin/autcel\
+	48 50 ../files/input/input_50.txt -o ../files/output/out_48_50_s
+
+./../bin/autcel\
+	245 50 ../files/input/input_50.txt -o ../files/output/out_245_50_s
+
+./..bin/autcel\
+	210 50 ../files/input/input_50.txt -o ../files/output/out_210_50_s
