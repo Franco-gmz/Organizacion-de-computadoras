ebee73069c38e1e11b32e240bf384f3fb76fed70
Franco-gmz <fdgomez@fi.uba.ar>
2021-06-29 16:34:55 -0300

    Actualizo prueba del bloque

--- TP2/test/unit/block.test.c
+++ TP2/test/unit/block.test.c
@@ -2,29 +2,46 @@
 #include"../../lib/Block.h"
 
 int testWRByte(int block_size,char* data){
-
+	printf("\nTEST:testWRByte");
+	printf("\nCreando bloque de tamaño %dB...\n",block_size);
 	Block* block = new_block(block_size); 
 	int result = 0;
 	for(int i = 0; i<block_size; i++){
-		write_byte(block,i,data[i]);
+		printf("\tEscribiendo byte#%d...\n",i+1);
+		block->data[i] = data[i];
 	}
 
 	for(int i = 0; i<block_size; i++){
-		char data_read = read_byte(block,i);
-		if(data_read != data[i]) result = -1;
+		char data_read = block->data[i];
+		if(data_read != data[i]){
+			printf("\tLectura del byte#%d incorrecta",i+1);
+			result = -1;
+		}
+		else printf("\tLectura del byte#%d correcta\n",i+1);
 	}
+	printf("\nLiberando bloque...\n");
 	free_block(block);
+	if(result == 0) printf("Resultado: OK\n");
+	else printf("Resultado: Fallo\n");
 	return result;
 }
 
 int testWRBlock(int block_size,char* data){
-
+	printf("\nTEST:testWRBlock");
+	printf("\nCreando bloque de tamaño %dB...\n",block_size);
 	Block* block = new_block(block_size);
 	int result = 0;
-	write_block(block,data);
-	char* read_data = read_block(block);
-	if(read_data == data) result = -1;
+	printf("Escribiendo bloque...\n");
+	block->data = data;
+	char* read_data = block->data;
+	if(read_data != data){
+		result = -1;
+	}
+	else printf("Lectura del bloque correcta\n");
+	printf("\nLiberando bloque...\n");
 	free_block(block);
+	if(result == 0) printf("Resultado: OK\n");
+	else printf("Resultado: Fallo\n");
 	return result;
 }
 
@@ -32,12 +49,7 @@ int main(){
 
 
 	char data[6] = {'B','l','o','q','u','e'};
-
-	if(testWRByte(6,data) == -1) printf("\nTest Write/Read byte : Fail\n");
-	else printf("\n Test Write/Read byte : OK\n");
-
-	if(testWRBlock(6,data) == -1) printf("\nTest Write/Read block : Fail\n");
-	else printf("\n Test Write/Read block : OK\n");
-
+	testWRByte(6,data);
+	testWRBlock(6,data);
 	return 0;
 }
